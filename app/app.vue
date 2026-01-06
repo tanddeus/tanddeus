6479ebd3f91cdc9dc97d7fef00e64ddf11bcf4b5
Joseph Dvorak <dvorakjt@gmail.com>
2026-01-06 02:12:40 -0800

    implemented page transitions

--- app/app.vue
+++ app/app.vue
@@ -5,4 +5,16 @@
 
 <style lang="scss">
 @use '~/assets/scss/main';
+.page-leave-active {
+  transition: opacity 150ms ease-in;
+}
+
+.page-enter-active {
+  transition: opacity 150ms ease-out;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@hypernym/nuxt-gsap'],
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+    },
+  },
 });
